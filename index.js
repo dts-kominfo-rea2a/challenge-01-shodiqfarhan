94225e6960aa0b6bd79a30e780b0ba1a04037124
Shodiq Farhan Jamil <shodiqfarhan24@gmail.com>
2022-05-28 12:11:19 +0700

    feat: ini chalenge struktur data

--- index.js
+++ index.js
@@ -1,20 +1,124 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+let colorSet = new Set();
+let restaurantSet = new Set();
+let color = {
+  yellow: "Yellow",
+  pink: "Pink",
+  white: "White",
+  purple: "Purple",
+  blue: "Blue",
+  black: "Black",
+  grey: "Grey",
+};
+let restaurant = {
+  bento: "Bento",
+  sushi: "Sushi",
+  pancake: "Pancake",
+  eggy: "Eggy",
+  tempura: "Tempura",
+  padang: "Padang",
+  tteok: "Tteok",
+  geprek: "Geprek",
+  katsu: "Katsu",
+};
+const firstUser = {
+  name: "Monica",
+  gender: "Female",
+  age: 17,
+  email: "monica@dingdong.com",
+  favoriteColor: colorSet
+    .add(color.yellow)
+    .add(color.pink)
+    .add(color.white)
+    .add(color.purple),
+  isHavePet: "Yes",
+  education: [
+    {
+      name: "SD 01",
+      city: "Jakarta",
+      graduate: 2016,
+    },
+    {
+      name: "SMP 02",
+      city: "Jakarta",
+      graduate: 2019,
+    },
+    {
+      name: "SMA 03",
+      city: "Tangerang",
+    },
+  ],
+  favoriteRestaurant: restaurantSet
+    .add(restaurant.bento)
+    .add(restaurant.sushi)
+    .add(restaurant.pancake)
+    .add(restaurant.eggy)
+    .add(restaurant.tempura)
+    .add(restaurant.bento)
+    .add(restaurant.eggy)
+    .add(restaurant.padang)
+    .add(restaurant.tteok)
+    .add(restaurant.sushi)
+    .add(restaurant.sushi),
+};
+
+const secondUser = {
+  name: "Wendy",
+  gender: "Male",
+  age: 23,
+  email: "wendy@dingdong.com",
+  favoriteColor: colorSet.add(color.blue).add(color.black).add(color.grey),
+  isHavePet: "No",
+  education: [
+    {
+      name: "SD 02",
+      city: "Jakarta",
+      graduate: 2010,
+    },
+    {
+      name: "SMP 03",
+      city: "Bogor",
+      graduate: 2013,
+    },
+    {
+      name: "SMA 01",
+      city: "Surabaya",
+      graduate: 2016,
+    },
+    {
+      name: "Universitas Maju",
+      city: "Tangerang",
+    },
+  ],
+  favoriteRestaurant: restaurantSet
+    .add(restaurant.bento)
+    .add(restaurant.sushi)
+    .add(restaurant.pancake)
+    .add(restaurant.eggy)
+    .add(restaurant.tempura)
+    .add(restaurant.bento)
+    .add(restaurant.eggy)
+    .add(restaurant.padang)
+    .add(restaurant.tteok)
+    .add(restaurant.sushi)
+    .add(restaurant.sushi),
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
+console.log(users);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
-    console.log(users.length || users.size);
-    console.log(users);
+  console.log(users.length || users.size);
+  console.log(users);
 }
 
 main();
 
 module.exports = {
-    users
+  users,
 };
